d7a2a6992e7f119db1f9b638c0dae05231425d7f
Enrico Melis <melchiah91@gmail.com>
2019-04-30 15:39:58 +0200

    Impostata connessione a db e server remoti

--- application/views/gestioneClienti.php
+++ application/views/gestioneClienti.php
@@ -11,9 +11,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
         <style>
-            table {
-                width: 100%
-            }
+            
             table, th, td {
               border: 1px solid black;
             }
@@ -32,6 +30,10 @@ and open the template in the editor.
                 background-color: lightgray;
                 color: black;
             }
+            #filtro{
+                display: inline;
+                margin-left: 20px;
+            }
         </style>
         
     </head>
@@ -40,18 +42,19 @@ and open the template in the editor.
             $items = "";
             foreach($sedi as $s)
             {
-                $items.="<option value='".$s->indirizzo."'>".$s->indirizzo."</option>";
+                $items.="<option value='".$s->id_sede."'>".$s->indirizzo."</option>";
             }
-                $selectSede = "<select>".$items."</select>";
+            $selectSede = "<select>".$items."</select>";
 
         
-        
+            $filtraSede = "<select id='filtraSede'><option value='all'>Tutte</option>".$items."</select>";
         
             if (count($appuntamenti) == 0)
             {
-                echo "Nessun cliente ha chiamato.<br>";
+                echo "<div id='noClienti'>Nessun cliente ha chiamato.<br></div>";
             }
             echo "<button class='addUser'>Aggiungi operazione</button>";
+            echo "<div id='filtro'>Filtra per sede: ".$filtraSede."</div>";
             //creo una tabella per contenere i dati
             echo "<table id='clienti'>";
                 
@@ -59,13 +62,13 @@ and open the template in the editor.
                 echo"<tr>";
                     echo "<th>Data inserimento</th>";
                     echo "<th>Cliente</th>";
+                    echo "<th>Trattamento</th>";
                     echo "<th>Sede</th>";
                     echo "<th>Risposto</th>";
                     echo "<th>Messaggio</th>";
                     echo "<th>Fissato app</th>";
                     echo "<th>Data/Ora app</th>";
                     echo "<th>Operatrice</th>";
-                    echo "<th>Trattamento</th>";
                     echo "<th>Pacc venduto</th>";
                     echo "<th>Num pacc</th>";
                 echo "</tr>";
@@ -81,13 +84,13 @@ and open the template in the editor.
                     $chiamataFormatted = new DateTime($row->data_ora_ch);
                     $array['chiamata'] = $chiamataFormatted;
                     $array['nome'] = "$row->nome $row->numero";
+                    $array['trat'] = $row->trattamento;
                     $array['sede'] = $row->sede;
                     $array['risp'] = $row->risp_tel;
                     $array['mess'] = $row->risp_mess;
                     $array['app'] = $row->fiss_app;
                     $array['data'] = $row->data_ora_app;
                     $array['op'] = $row->operatrice;
-                    $array['trat'] = $row->trattamento;
                     $array['pacc'] = $row->vend_pac;
                     $array['idPacc'] = $row->id_pacc;
                     $array['nomePacc'] = $row->nome_pacc;
@@ -113,9 +116,11 @@ and open the template in the editor.
                             else if ($key == "nome")
                             {
                                 echo "<form class='nomeCliente'>";
-                                echo "<input type='text' class='insNomeCliente' required>";
-                                echo "<input type='text' class='insNumCliente' required>";
-                                echo "<button type='button' id='submitNomeCliente'>Inserisci</button>";
+                                echo "<label for='insNomeCliente'>Nome</label>";
+                                echo "<input type='text' class='insNomeCliente' id='insNomeCliente' required><br>";
+                                echo "<label for='insNumCliente'>Numero</label>";
+                                echo "<input type='text' class='insNumCliente'id='insNumCliente' required><br>";
+                                echo "<button type='button' id='submitCliente'>Inserisci</button>";
                                 echo "</form>";
                             }
                             else if ($key == "op")
@@ -150,10 +155,8 @@ and open the template in the editor.
                             //negli altri casi inserisco due tasti SI e NO
                             else
                             {
-                                echo "<button class='si' ";
-                                echo ">SI</button>";
-                                echo "<button class='no'";
-                                echo ">NO</button>";
+                                echo "<button class='si'>SI</button>";
+                                echo "<button class='no'>NO</button>";
 
                                 //se la colonna è relativa alla vendita del pacchetto
                                 if ($key == "pacc")
@@ -181,9 +184,18 @@ and open the template in the editor.
             
             <td class='nome'>
                 <form class='nomeCliente'>
-                    <input type='text' class='insNomeCliente' required>
-                    <input type='text' class='insNumCliente' required>
-                    <button type='button' id='submitNomeCliente'>Inserisci</button>
+                    <label for='insNomeCliente'>Nome</label>
+                    <input type='text' class='insNomeCliente' id='insNomeCliente' required><br>
+                    <label for='insNumCliente'>Numero</label>
+                    <input type='text' class='insNumCliente' id='insNumCliente' required><br>
+                    <button type='button' id='submitCliente'>Inserisci</button>
+                </form>
+            </td>
+            
+            <td class='trat'>
+                <form class='nomeTrat'>
+                    <input type='text' class='insNomeTrat' required>
+                    <button type='button' id='submitNomeTrat'>Inserisci</button>
                 </form>
             </td>
             
@@ -220,13 +232,6 @@ and open the template in the editor.
                 </form>
             </td>
             
-            <td class='trat'>
-                <form class='nomeTrat'>
-                    <input type='text' class='insNomeTrat' required>
-                    <button type='button' id='submitNomeTrat'>Inserisci</button>
-                </form>
-            </td>
-            
             <td class='pacc'>
                 <button class='si'>SI</button>
                 <button class='no'>NO</button>
@@ -242,9 +247,10 @@ and open the template in the editor.
         ?>
         <script> 
             //converto le variabili php in oggetti json gestibili da JS
+            var listaSedi = <?php echo json_encode($sedi); ?>;
             var results = <?php echo json_encode($appuntamenti); ?>;
             var lastPac = <?php echo json_encode($ultimoPacc); ?>;
-            var riga = <?php echo json_encode($riga); ?>;
+            var nuovaRiga = <?php echo json_encode($riga); ?>;
             //infine carico JQuery e lo script di gestione del registro
         </script>
         <script src="<?php echo base_url()."assets/js/jquery-3.4.0.js"?>"></script>
