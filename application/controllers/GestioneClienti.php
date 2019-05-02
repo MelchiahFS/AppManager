a8f3cb7b833ac43d6eab8fcd771af991927bc2bb
Enrico Melis <melchiah91@gmail.com>
2019-05-02 13:50:11 +0200

    rimossi header in Controller

--- application/controllers/GestioneClienti.php
+++ application/controllers/GestioneClienti.php
@@ -1,7 +1,7 @@
 <?php
 
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache');
+//header('Content-Type: text/event-stream');
+//header('Cache-Control: no-cache');
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
