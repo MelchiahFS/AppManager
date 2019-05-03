dcec9bcd50a8313dd0c5e5ec95f2cbd9786ebec5
Enrico Melis <melchiah91@gmail.com>
2019-05-03 17:17:36 +0200

    Test url dinamico AJAX

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
-//$config['base_url'] = 'https://intranetapp.doctorloveskin.it/';
+//$config['base_url'] = '';
+$config['base_url'] = 'http://intranetapp.doctorloveskin.it/';
 
 /*
 |--------------------------------------------------------------------------
